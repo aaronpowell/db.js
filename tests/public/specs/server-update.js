d785fb7c8a0a3a78bc56d0130f916b2ded62ec5d
Aaron Powell <me@aaron-powell.com>
2012-10-07 21:56:09 +1100

    tests with custom keys

--- tests/public/specs/server-update.js
+++ tests/public/specs/server-update.js
@@ -217,7 +217,158 @@
             waitsFor(function () {
                 return done;
             } , 'timed out waiting for get' , 1000 );
+        });
 
+    });
+
+    describe( 'server.update-custom-keys' , function () {
+        var dbName = 'tests',
+            indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.oIndexedDB || window.msIndexedDB;
+           
+       beforeEach( function () {
+            var done = false;
+            var spec = this;
+            
+            spec.server = undefined;
+            
+            runs( function () {
+                var req = indexedDB.deleteDatabase( dbName );
+                
+                req.onsuccess = function () {
+                    done = true;
+                };
+                
+                req.onerror = function () {
+                    console.log( 'failed to delete db' , arguments );
+                };
+                
+                req.onblocked = function () {
+                    console.log( 'db blocked' , arguments , spec );
+                };
+            });
+            
+            waitsFor( function () {
+                 return done;
+            }, 'timed out deleting the database', 1000);
+            
+            runs( function () {
+                db.open( {
+                    server: dbName ,
+                    version: 1 ,
+                    schema: { 
+                        test: {
+                        }
+                    }
+                }).done(function ( s ) {
+                    spec.server = s;
+                });
+            });
+            
+            waitsFor( function () { 
+                return !!spec.server;
+            } , 'wait on db' , 500 );
+        });
+        
+        afterEach( function () {
+            var done;
+
+            runs( function () {
+                if ( this.server ) {
+                    this.server.close();
+                }                
+                var req = indexedDB.deleteDatabase( dbName );
+                
+                req.onsuccess = function () {
+                    done = true;
+                };
+                
+                req.onerror = function () {
+                    console.log( 'failed to delete db' , arguments );
+                };
+                
+                req.onblocked = function () {
+                    console.log( 'db blocked' , arguments );
+                };
+            });
+            
+            waitsFor( function () {
+                 return done;
+            }, 'timed out deleting the database', 1000);
+        });
+
+        it( 'should allow updating with custom keys' , function () {
+            var item = {
+                firstName: 'Aaron',
+                lastName: 'Powell'
+            };
+            var key = 'foo';
+            
+            var spec = this;
+            var done;
+
+            runs( function () {
+                spec.server
+                .add( 'test' , {
+                        item: item,
+                        key: key
+                    })
+                .done( function ( records ) {
+                    done = true;
+                });
+            });
+            
+            waitsFor( function () {
+                return done;
+            } , 'timeout waiting for item to be added' , 1000 );
+            
+            var done;
+
+            runs( function () {
+                done = false;
+
+                item.firstName = 'John';
+                item.lastName = 'Smith';
+
+                spec.server
+                    .test
+                    .update( {
+                        item: item,
+                        key: key
+                    } )
+                    .done( function ( records ) {
+                        done = true;
+                    });
+            });
+
+            waitsFor(function () {
+                return done;
+            } , 'timed out waiting for update' , 1000 );
+
+            runs( function () {
+                done = false;
+
+                spec.server
+                    .test
+                    .query()
+                    .all()
+                    .execute()
+                    .done( function ( records ) {
+                        done = true;
+
+                        expect( records.length ).toBe( 1 );
+
+                        var record = records[0];
+                        expect( record ).toBeDefined();
+                        expect( record.__id__ ).toBe( key );
+                        expect( record.firstName ).toBe( item.firstName );
+                        expect( record.lastName ).toBe( item.lastName );
+                        expect( record ).not.toBe( item );
+                    });
+            });
+
+            waitsFor(function () {
+                return done;
+            } , 'timed out waiting for get' , 1000 );
         });
     });
 
