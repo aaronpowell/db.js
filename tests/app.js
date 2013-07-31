60e54870e868d35e5edd9c6675d8677c109b5296
Aaron Powell <me@aaron-powell.com>
2013-08-01 09:37:43 +1000

    updating the test runner

--- tests/app.js
+++ tests/app.js
@@ -1,15 +1,16 @@
 var express = require('express');
-var app = module.exports = express.createServer();
+var app = module.exports = express();
 var fs = require('fs');
+var path = require('path');
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+app.set('views', path.join(__dirname, 'views'));
+app.use('/lib', express.static(path.join(__dirname, 'lib')));
+app.use('/specs', express.static(path.join(__dirname, 'specs')));
+app.set('view engine', 'jade');
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
 app.configure('development', function(){
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
@@ -28,7 +29,7 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.get('/db.js', function (req, res) {
+app.get('/src/db.js', function (req, res) {
 	fs.readFile(__dirname + '/../src/db.js', function ( err , data ) {
 		res.type( 'application/javascript' );
 		res.send( 200 , data );
