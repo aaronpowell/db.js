a5c944f5b894d088a181793f86037d6f4f86cfa9
Maurice de Beijer <maurice.de.beijer@gmail.com>
2013-10-13 21:57:37 +0200

    Allow adding indexes to an existing object store

--- tests/specs/indexes.js
+++ tests/specs/indexes.js
@@ -114,6 +114,74 @@
                 return done;
             } , 1000 , 'timed out running specs' );
         });
-    });
+
+        it( 'should allow adding indexes to an existing object store' , function () {
+            var spec = this;
+            runs( function () {
+                db.open( {
+                    server: dbName ,
+                    version: 1,
+                    schema: { 
+                        test: {
+                            key: {
+                                keyPath: 'id',
+                                autoIncrement: true
+                            },
+                        }
+                    }
+                }).done(function ( s ) {
+                    s.close();
+					
+                    db.open( {
+                        server: dbName,
+                        version: 2,
+						schema: { 
+							test: {
+								key: {
+									keyPath: 'id',
+									autoIncrement: true
+								},
+								indexes: {
+									firstName: { },
+									age: { }
+								}
+							}
+						}
+                    }).done(function ( s ) {
+						spec.server = s;
+                    });
+                });
+            });
+
+            waitsFor( function () {
+                return !!spec.server;
+            } , 1000 , 'timed out opening the db' );
+			
+            var done = false;
+            runs( function () {
+                spec.server.close();
+
+                var req = indexedDB.open( dbName , 2 );
+                req.onsuccess = function ( e ) {
+                    var res = e.target.result;
+
+                    var transaction = res.transaction( 'test' );
+                    var store = transaction.objectStore( 'test' );
+                    var indexNames = Array.prototype.slice.call( store.indexNames );
+
+                    expect( indexNames.length ).toEqual( 2 );
+                    expect( indexNames ).toContain( 'firstName' );
+                    expect( indexNames ).toContain( 'age' );
+
+                    spec.server = res;
+                    done = true;
+                };
+            });
+
+            waitsFor( function () {
+                return done;
+            } , 1000 , 'timed out running specs' );
+        });
+		});
 
 })( window.db , window.describe , window.it , window.runs , window.expect , window.waitsFor , window.beforeEach , window.afterEach );
