5a77c963122a2c9c3981b6c3636c50c774408f41
Yonggang Luo <luoyonggang@gmail.com>
2015-03-12 06:49:55 +0800

    Fixes of tests in indexes.js, server-remove.js, thenable-integration.js

--- tests/specs/indexes.js
+++ tests/specs/indexes.js
@@ -1,73 +1,93 @@
-(function ( db , describe , it , runs , expect , waitsFor , beforeEach , afterEach ) {
+(function ( db , describe , it , expect , beforeEach , afterEach ) {
     'use strict';
-    
+
     describe( 'db.indexes' , function () {
         var dbName = 'tests',
-            indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.oIndexedDB || window.msIndexedDB;
+          indexedDB = db.indexedDB;
            
-       beforeEach( function () {
-            var done = false;
+        beforeEach( function (done) {
             var spec = this;
             
             spec.server = undefined;
             
-            runs( function () {
-                var req = indexedDB.deleteDatabase( dbName );
-                
-                req.onsuccess = function () {
-                    done = true;
-                };
-                
-                req.onerror = function () {
-                    console.log( 'failed to delete db in beforeEach' , arguments );
-                };
-                
-                req.onblocked = function () {
-                    console.log( 'db blocked' , arguments , spec );
-                };
+            db.remove(dbName).then(done, function(err) {
+                console.log( 'failed to delete db' , arguments );
+                done(err);
             });
-            
-            waitsFor( function () {
-                 return done;
-            }, 'timed out deleting the database', 1000);
         });
         
-        afterEach( function () {
-            var done;
-
-            runs( function () {
-                if ( this.server ) {
-                    this.server.close();
-                }
-
-                var spec = this;
-
-                var req = indexedDB.deleteDatabase( dbName );
-
-                req.onsuccess = function () {
-                    done = true;
-                };
-                
-                req.onerror = function () {
-                    console.log( 'failed to delete db in afterEach' , arguments , spec );
-                };
-                
-                req.onblocked = function () {
-                    console.log( 'db blocked' , arguments );
-                };
+        afterEach( function (done) {
+            if ( this.server ) {
+                this.server.close();
+            }
+            db.remove(dbName).then(function(){
+                done();
+            }, function(err) {
+                console.log( 'failed to delete db' , arguments );
+                done(err);
             });
-            
-            waitsFor( function () {
-                 return done;
-            }, 'timed out deleting the database', 1000);
         });
         
-        it( 'should allow creating dbs with indexes' , function () {
+        it( 'should allow creating dbs with indexes' , function (done) {
             var spec = this;
-            runs( function () {
+            db.open( {
+                server: dbName ,
+                version: 1,
+                schema: { 
+                    test: {
+                        key: {
+                            keyPath: 'id',
+                            autoIncrement: true
+                        },
+                        indexes: {
+                            firstName: { },
+                            age: { }
+                        }
+                    }
+                }
+            }).then(function ( s ) {
+                spec.server = s;
+                next();
+            });
+
+            function next() {
+                spec.server.close();
+                var req = indexedDB.open( dbName , 1 );
+                req.onsuccess = function ( e ) {
+                    var res = e.target.result;
+
+                    var transaction = res.transaction( 'test' );
+                    var store = transaction.objectStore( 'test' );
+                    var indexNames = Array.prototype.slice.call( store.indexNames );
+
+                    expect( indexNames.length ).toEqual( 2 );
+                    expect( indexNames ).toContain( 'firstName' );
+                    expect( indexNames ).toContain( 'age' );
+
+                    spec.server = res;
+                    done();
+                };
+            }
+        });
+
+        it( 'should allow adding indexes to an existing object store' , function (done) {
+            var spec = this;
+            db.open( {
+                server: dbName ,
+                version: 1,
+                schema: { 
+                    test: {
+                        key: {
+                            keyPath: 'id',
+                            autoIncrement: true
+                        },
+                    }
+                }
+            }).then(function ( s ) {
+                s.close();
                 db.open( {
-                    server: dbName ,
-                    version: 1,
+                    server: dbName,
+                    version: 2,
                     schema: { 
                         test: {
                             key: {
@@ -82,83 +102,11 @@
                     }
                 }).then(function ( s ) {
                     spec.server = s;
+                    next();
                 });
             });
 
-            waitsFor( function () {
-                return !!spec.server;
-            } , 1000 , 'timed out opening the db' );
-
-            var done = false;
-            runs( function () {
-                spec.server.close();
-
-                var req = indexedDB.open( dbName , 1 );
-                req.onsuccess = function ( e ) {
-                    var res = e.target.result;
-
-                    var transaction = res.transaction( 'test' );
-                    var store = transaction.objectStore( 'test' );
-                    var indexNames = Array.prototype.slice.call( store.indexNames );
-
-                    expect( indexNames.length ).toEqual( 2 );
-                    expect( indexNames ).toContain( 'firstName' );
-                    expect( indexNames ).toContain( 'age' );
-
-                    spec.server = res;
-                    done = true;
-                };
-            });
-
-            waitsFor( function () {
-                return done;
-            } , 1000 , 'timed out running specs' );
-        });
-
-        it( 'should allow adding indexes to an existing object store' , function () {
-            var spec = this;
-            runs( function () {
-                db.open( {
-                    server: dbName ,
-                    version: 1,
-                    schema: { 
-                        test: {
-                            key: {
-                                keyPath: 'id',
-                                autoIncrement: true
-                            },
-                        }
-                    }
-                }).then(function ( s ) {
-                    s.close();
-					
-                    db.open( {
-                        server: dbName,
-                        version: 2,
-						schema: { 
-							test: {
-								key: {
-									keyPath: 'id',
-									autoIncrement: true
-								},
-								indexes: {
-									firstName: { },
-									age: { }
-								}
-							}
-						}
-                    }).then(function ( s ) {
-						spec.server = s;
-                    });
-                });
-            });
-
-            waitsFor( function () {
-                return !!spec.server;
-            } , 1000 , 'timed out opening the db' );
-			
-            var done = false;
-            runs( function () {
+            function next() {
                 spec.server.close();
 
                 var req = indexedDB.open( dbName , 2 );
@@ -174,21 +122,33 @@
                     expect( indexNames ).toContain( 'age' );
 
                     spec.server = res;
-                    done = true;
+                    done();
                 };
-            });
-
-            waitsFor( function () {
-                return done;
-            } , 1000 , 'timed out running specs' );
+            }
         });
 
-        it( 'should allow adding indexes to an existing object store with indexes' , function () {
+        it( 'should allow adding indexes to an existing object store with indexes' , function (done) {
             var spec = this;
-            runs( function () {
+            db.open( {
+                server: dbName ,
+                version: 1,
+                schema: { 
+                    test: {
+                        key: {
+                            keyPath: 'id',
+                            autoIncrement: true
+                        },
+                        indexes: {
+                            firstName: {}
+                        }
+                    }
+                }
+            }).then(function ( s ) {
+                s.close();
+                
                 db.open( {
-                    server: dbName ,
-                    version: 1,
+                    server: dbName,
+                    version: 2,
                     schema: { 
                         test: {
                             key: {
@@ -196,40 +156,18 @@
                                 autoIncrement: true
                             },
                             indexes: {
-                                firstName: {}
+                                firstName: { },
+                                age: { }
                             }
                         }
                     }
                 }).then(function ( s ) {
-                    s.close();
-                    
-                    db.open( {
-                        server: dbName,
-                        version: 2,
-                        schema: { 
-                            test: {
-                                key: {
-                                    keyPath: 'id',
-                                    autoIncrement: true
-                                },
-                                indexes: {
-                                    firstName: { },
-                                    age: { }
-                                }
-                            }
-                        }
-                    }).then(function ( s ) {
-                        spec.server = s;
-                    });
+                    spec.server = s;
+                    next();
                 });
             });
 
-            waitsFor( function () {
-                return !!spec.server;
-            } , 1000 , 'timed out opening the db' );
-            
-            var done = false;
-            runs( function () {
+            function next() {
                 spec.server.close();
 
                 var req = indexedDB.open( dbName , 2 );
@@ -245,14 +183,9 @@
                     expect( indexNames ).toContain( 'age' );
 
                     spec.server = res;
-                    done = true;
+                    done();
                 };
-            });
-
-            waitsFor( function () {
-                return done;
-            } , 1000 , 'timed out running specs' );
+            }
         });
-		});
-
-})( window.db , window.describe , window.it , window.runs , window.expect , window.waitsFor , window.beforeEach , window.afterEach );
+    });
+})( window.db , window.describe , window.it , window.expect , window.beforeEach , window.afterEach );
